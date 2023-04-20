3a908e1c6e66d1da832c7b3bab36b1e2b4b77872
mateusferian <mateusferian10@gmail.com>
2023-04-20 11:26:47 -0300

    :tada: initial commit

---
+++ src/main/java/br/com/geradordedevs/uolplayers/UolPlayersApplication.java
@@ -0,0 +1,13 @@
+package br.com.geradordedevs.uolplayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolPlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolPlayersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/geradordedevs/uolplayers/UolPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geradordedevs.uolplayers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolPlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
